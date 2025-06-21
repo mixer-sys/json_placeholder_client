aedeee4e6c83ba6f086b58e648081b759a8ec44e
Grigory <you@example.com>
2025-06-21 12:06:20 -0400

    Made http server

--- go.mod
+++ go.mod
@@ -1,6 +1,5 @@
 module github.com/mixer-sys/json_placeholder_client
 
-
 go 1.24.3
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
--- internal/app/client/client.go
+++ internal/app/client/client.go
@@ -6,11 +6,17 @@ import (
 	"net/url"
 	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/mixer-sys/json_placeholder_client/internal/app/handlers"
 	"github.com/mixer-sys/json_placeholder_client/internal/app/logger"
 )
 
 func GetProxyURL() (*url.URL, error) {
 	log := logger.GetLogger()
+	err := godotenv.Load()
+	if err != nil {
+		log.Info.Println("Error loading .env file")
+	}
 	proxyStr := os.Getenv("PROXY_URL")
 	if proxyStr == "" {
 		log.Error.Println("PROXY_URL is not set in environment variables")
@@ -38,6 +44,31 @@ func GetClient() (*http.Client, error) {
 		Proxy: http.ProxyURL(proxyURL),
 	}
 
-	client := &http.Client{Transport: transport}
+	client := &http.Client{Transport: transport, CheckRedirect: func(req *http.Request, via []*http.Request) error {
+		return nil
+	},
+	}
 	return client, nil
 }
+
+func RunTestClient() {
+	log := logger.GetLogger()
+
+	client, err := GetClient()
+	if err != nil {
+		log.Error.Println("Error creating HTTP client:", err)
+		return
+	}
+
+	posts := handlers.GetPosts(client)
+	for _, post := range posts {
+		log.Info.Printf("Post ID: %d, Title: %s", post.ID, post.Title)
+	}
+	postID := 1 // Example post ID
+	post := handlers.GetPostByID(client, postID)
+	log.Info.Printf("Post ID: %d, Title: %s, Body: %s", post.ID, post.Title, post.Body)
+	log.Info.Println("Test client run completed successfully.")
+
+}
+
+//
